fba701acbbabccf1b66b90c96817ffdee0d2417b
Artur <tuchowskyy@gmail.com>
2019-01-31 21:11:55 +0100

    feat(home): working rezise on device move and change photo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
-import { Component, HostListener, ElementRef, ViewChild } from '@angular/core';
-
+import { Component, HostListener, ElementRef, ViewChild, OnInit, OnDestroy } from '@angular/core';
 
 
 @Component({
@@ -7,27 +6,46 @@ import { Component, HostListener, ElementRef, ViewChild } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
  
-  // newStyle = {
-  //   'background-size': '500%'
-  // }
+  private screenHeight: number;
+  private screenTotalHeight: number;
+  private sectionsCount: number;
+  private onePercentOfScreenHeight: number;
 
   constructor(private el: ElementRef) {
+    this.getScreenSize();
   }
 
   @ViewChild("maincont", {read: ElementRef}) maincont: ElementRef
 
   @HostListener('scroll', ['$event']) 
+  @HostListener('window:resize', ['$event'])
+
   scrollHandler($event) {
-    //let element = $event.target.all[15];
-    console.log($event.target.scrollTop);
-    let newSize = (500 - $event.target.scrollTop * 0.205);
-    console.log(newSize);
-    // this.newStyle["background-size"] = '"auto ' +  newSize + '%"';
+    let newSize = (500 - $event.target.scrollTop * this.onePercentOfScreenHeight);
     let size = 'background-size: auto ' +  newSize + '%';
     this.maincont.nativeElement.setAttribute('style', size);
   }
 
+  onResize($event?) {
+    console.log($event.target.innerHeight);
+    this.screenHeight = $event.target.innerHeight;
+    this.screenTotalHeight = this.screenHeight*3;
+    this.onePercentOfScreenHeight = this.sectionsCount/this.screenTotalHeight;
+  }
+
+  getScreenSize(event?) {
+    console.log(window.innerHeight);
+    this.screenHeight = window.innerHeight;
+    this.screenTotalHeight = this.screenHeight*3;
+    this.sectionsCount = 4*100;
+    this.onePercentOfScreenHeight = this.sectionsCount/this.screenTotalHeight;
+  }
+
+
+  ngOnInit(): void {
+    
+  }
 
 }
---
+++ src/app/services/resize.service.ts
@@ -0,0 +1,9 @@
+// import { Injectable } from '@angular/core';
+// import { Subject, Observable, BehaviorSubject } from 'rxjs';
+
+// @Injectable()
+// export class ResizeService {
+
+
+// }
+
