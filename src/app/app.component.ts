5fa8d191d399c94a930ebb5d19420c8e7a81b265
Artur <tuchowskyy@gmail.com>
2019-01-31 01:33:33 +0100

    feat(main): scrolling and resizing background works, add updating height when resize

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, HostListener, ElementRef, ViewChild } from '@angular/core';
 
 
 
@@ -9,13 +9,24 @@ import { Component, HostListener } from '@angular/core';
 })
 export class AppComponent {
  
-  constructor() {
+  // newStyle = {
+  //   'background-size': '500%'
+  // }
+
+  constructor(private el: ElementRef) {
   }
+
+  @ViewChild("maincont", {read: ElementRef}) maincont: ElementRef
 
   @HostListener('scroll', ['$event']) 
   scrollHandler($event) {
     //let element = $event.target.all[15];
-    console.log($event);
+    console.log($event.target.scrollTop);
+    let newSize = (500 - $event.target.scrollTop * 0.205);
+    console.log(newSize);
+    // this.newStyle["background-size"] = '"auto ' +  newSize + '%"';
+    let size = 'background-size: auto ' +  newSize + '%';
+    this.maincont.nativeElement.setAttribute('style', size);
   }
 
 
