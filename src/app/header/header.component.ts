536c226db5b080ae06b9dc832336ecca33e2d0fa
Artur <tuchowskyy@gmail.com>
2019-02-02 17:55:29 +0100

    feat(header): working menu toggling on small devides

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  menuOpen = false;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onShowMenu() {
+    this.menuOpen = !this.menuOpen;
   }
 
 }
